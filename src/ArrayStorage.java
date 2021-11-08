e713f4e6f3be2e9b93380dfde8b33982ee6a7518
Alex D <ali.email@mail.ru>
2021-11-08 19:52:38 +0300

    correction h/w#1

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -5,21 +5,22 @@ import java.util.Arrays;
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
+    private int size;
 
     void clear() {
-        for (int i = 0; i < size(); i++) {
+        for (int i = 0; i < size; i++) {
             storage[i] = null;
         }
+        size = 0;
     }
 
     void save(Resume r) {
-        storage[size()] = r;
+        storage[size] = r;
+        size++;
     }
 
     Resume get(String uuid) {
-        int i;
-        int size = size();
-        for (i = 0; i <= size; i++) {
+        for (int i = 0; i < size; i++) {
             if (storage[i] != null && storage[i].uuid.equals(uuid)) {
                 return storage[i];
             }
@@ -28,11 +29,11 @@ public class ArrayStorage {
     }
 
     void delete(String uuid) {
-        int size = size();
-        for (int i = 0; i <= size; i++) {
+        for (int i = 0; i < size; i++) {
             if (storage[i] != null && storage[i].uuid.equals(uuid)) {
-                storage[i] = storage[size];
-                storage[size] = null;
+                storage[i] = storage[size - 1];
+                storage[size - 1] = null;
+                size--;
                 break;
             }
         }
@@ -42,17 +43,10 @@ public class ArrayStorage {
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        Resume [] allResume = Arrays.copyOf(storage, size());
-                return allResume;
+        return Arrays.copyOf(storage, size);
     }
 
     int size() {
-        int i;
-        for ( i = 0 ; i < storage.length; i++) {
-            if (storage[i] == null) {
-            break;
-            }
-        }
-        return i;
+        return size;
     }
 }
