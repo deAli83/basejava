46db1f8467dbb75644cea1c9744345d3f2002496
Alex D <ali.email@mail.ru>
2022-01-25 13:39:53 +0300

    cherry-pick commits

--- src/ru/javawebinar/basejava/storage/ArrayStorage.java
+++ src/ru/javawebinar/basejava/storage/ArrayStorage.java
@@ -8,16 +8,9 @@ import java.util.Arrays;
  * Array based storage for Resumes
  */
 public class ArrayStorage {
-<<<<<<< HEAD
-    private Resume[] storage = new Resume[10000];
-    private int size;
-=======
-
     private static final int STORAGE_LIMIT = 10000;
-
     private Resume[] storage = new Resume[STORAGE_LIMIT];
     private int size = 0;
->>>>>>> 9d8464c (Lesson03 equals&hashCode, static)
 
     public void clear() {
         Arrays.fill(storage, 0, size, null);
@@ -25,22 +18,18 @@ public class ArrayStorage {
     }
 
     public void update(Resume resume) {
-        int index = findIndex(resume.getUuid());
+        int index = getIndex(resume.getUuid());
         if (index != -1) {
             storage[index] = resume;
         }
     }
 
     public void save(Resume r) {
-<<<<<<< HEAD
-        if (size < storage.length && findIndex(r.getUuid()) == -1) {
-=======
         if (getIndex(r.getUuid()) != -1) {
             System.out.println("Resume " + r.getUuid() + " already exist");
         } else if (size >= STORAGE_LIMIT) {
             System.out.println("Storage overflow");
         } else {
->>>>>>> 9d8464c (Lesson03 equals&hashCode, static)
             storage[size] = r;
             size++;
             return;
@@ -49,7 +38,7 @@ public class ArrayStorage {
     }
 
     public Resume get(String uuid) {
-        int index = findIndex(uuid);
+        int index = getIndex(uuid);
         if (index != -1) {
             return storage[index];
         }
@@ -57,7 +46,7 @@ public class ArrayStorage {
     }
 
     public void delete(String uuid) {
-        int index = findIndex(uuid);
+        int index = getIndex(uuid);
         if (index != -1) {
             size--;
             storage[index] = storage[size];
@@ -65,23 +54,20 @@ public class ArrayStorage {
         }
     }
 
-<<<<<<< HEAD
     /**
      * @return array, contains only Resumes in storage (without null)
      */
-    public Resume[] getAll() {
-        return Arrays.copyOf(storage, size);
-=======
+
     public Resume[] getAll() {
         return Arrays.copyOfRange(storage, 0, size);
->>>>>>> 9d8464c (Lesson03 equals&hashCode, static)
+
     }
 
     public int size() {
         return size;
     }
 
-    private int findIndex(String uuid) {
+    private int getIndex(String uuid) {
         for (int i = 0; i < size; i++) {
             if (storage[i].getUuid().equals(uuid)) {
                 System.out.println("Resume " + uuid + " find in storage");
