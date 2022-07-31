ad229eca1a77107ca227ac7434f7153166b7457b
Alex D <ali.email@mail.ru>
2022-08-01 02:20:55 +0300

    HW13. Start

---
+++ src/ru/javawebinar/basejava/sql/ConnectionFactory.java
@@ -0,0 +1,7 @@
+package ru.javawebinar.basejava.sql;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public interface ConnectionFactory {
+    Connection getConnection() throws SQLException;
+}
